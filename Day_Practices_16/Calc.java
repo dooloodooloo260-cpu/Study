6ab9c7dfdbb478c7f80984f9268ff21983fd7ca6
dooloodooloo26-cpu <dooloodooloo260@gmail.com>
2026-09-21 09:28:32 -0700

    add calc.java progress calculator

---
+++ Day_Practices_16/Calc.java
@@ -0,0 +1,9 @@
+public class Calc {
+    public static void main(String[] args) {
+        int days = 16;
+        int goals = 90; 
+        double percent = (double) days / goals
+* 100; 
+        System.out.println("Progress: " + percent + "%");
+    }
+}
