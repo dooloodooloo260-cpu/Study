81563df319ddd770673656555b5a4e1d203e90f4
dooloodooloo26-cpu <dooloodooloo260@gmail.com>
2026-09-22 13:48:32 -0700

    day 20 completed loopif

---
+++ Day_Practices_20/LoopIf.java
@@ -0,0 +1,13 @@
+package Day_Practices_20;
+
+public class LoopIf {
+    public static void main(String[]args) {
+        for (int i = 1; i <=19 ; i ++) {
+            if (i == 16)  {
+                System.out.println("Day" + i + " - completed!");
+            } else {
+                System.out.println("Day" + i );
+            }
+        }
+    }
+}
