63eb054e2fcfdc9856ebc7bf365a461848f7bff8
dooloodooloo26-cpu <dooloodooloo260@gmail.com>
2026-09-22 14:18:01 -0700

    day 20 homework loopif 02 done

---
+++ Day_Practices_20/LoopIf02.java
@@ -0,0 +1,11 @@
+
+
+public class LoopIf02 {
+    public static void  main (String[]args) {
+        for ( int i = 1; i <=20; i++ ) {
+            if (i % 5 == 0)
+            {System.out.println("Day" + i);
+            }
+        }
+    }
+}
