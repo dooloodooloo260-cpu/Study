0322b308f004aa50509da8c60e44c3c69067fe9b
dooloodooloo26-cpu <dooloodooloo260@gmail.com>
2026-09-23 09:08:17 -0700

    new update --

---
+++ Day_Practices_20/LoopIf03.java
@@ -0,0 +1,15 @@
+
+// 3. 1-10: i == 7 бол "Lucky 7!", бусдыг энгийнээр хэвлэ. 
+
+public class LoopIf03 {
+    public static void main( String[] args) {
+        for (int i = 1; i <= 11; i-- ) {
+            if (i == 6 ) {
+                System.out.println("Lucky6!");
+            } else {
+                System.out.println("Day"+ i);
+            }
+        }
+
+    }
+}
